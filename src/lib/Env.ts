ef952fd05f428a985be27d8f29c01bdff46a3b71
Dylan R. Johnston <Dylan.R.Johnston@gmail.com>
2019-11-11 09:37:24 +0800

    Add support for full cron variables

--- src/lib/Env.ts
+++ src/lib/Env.ts
@@ -1,15 +1,23 @@
-import "dotenv/config";
-import * as t from "io-ts";
-import { validate } from "./validate";
+import "dotenv/config"
+import * as t from "io-ts"
+import { validate } from "./validate"
 
 interface ENV_TYPE extends t.TypeOf<typeof ENV_TYPE> {}
-const ENV_TYPE = t.type({
-  WEBHOOK_ID: t.string,
-  WEBHOOK_TOKEN: t.string,
-  MESSAGE: t.string,
-  CRON_HOUR: t.string,
-  CRON_MINUTE: t.string,
-  APP_NAME: t.string
-});
+const ENV_TYPE = t.intersection([
+  t.type({
+    APP_NAME: t.string,
+    MESSAGE: t.string,
+    WEBHOOK_ID: t.string,
+    WEBHOOK_TOKEN: t.string,
+  }),
+  t.partial({
+    CRON_DAY: t.string,
+    CRON_HOUR: t.string,
+    CRON_MINUTE: t.string,
+    CRON_MONTH: t.string,
+    CRON_WEEKDAY: t.string,
+    CRON_YEAR: t.string,
+  }),
+])
 
-export const ENV: ENV_TYPE = validate(ENV_TYPE)(process.env);
+export const ENV: ENV_TYPE = validate(ENV_TYPE)(process.env)
--- src/lib/validate.ts
+++ src/lib/validate.ts
@@ -1,19 +1,14 @@
-import * as t from "io-ts";
-import { pipe } from "fp-ts/lib/pipeable";
-import { fold } from "fp-ts/lib/Either";
-import { identity } from "fp-ts/lib/function";
-import { failure } from "io-ts/lib/PathReporter";
+import * as t from "io-ts"
+import { pipe } from "fp-ts/lib/pipeable"
+import { fold } from "fp-ts/lib/Either"
+import { identity } from "fp-ts/lib/function"
+import { failure } from "io-ts/lib/PathReporter"
 
-export const onSuccess = identity;
+export const onSuccess = identity
 export const onFailure = (errors: t.Errors) => {
-  throw new Error(failure(errors).join("\n"));
-};
+  throw new Error(failure(errors).join("\n"))
+}
 
 export const validate = <T>(type: t.Decoder<unknown, T>) => (
-  data: unknown
-): T =>
-  pipe(
-    data,
-    type.decode,
-    fold(onFailure, onSuccess)
-  );
+  data: unknown,
+): T => pipe(data, type.decode, fold(onFailure, onSuccess))
